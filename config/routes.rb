102bdd5d8e561bce782a1eadf9ad7b6ca69bb8d7
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-15 18:48:45 +0900

    Tell the router to use Users controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: {
+    confirmations: 'users/confirmations'
+    omniauth_callbacks: 'users/omniauth_callbacks',
+    passwords: 'users/passwords'
+    registrations: 'users/registrations'
+    sessions: 'users/sessions'
+    unlocks: 'users/unlocks'
+  }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'templates#index'
   get '/templates/show' => 'templates#show'
