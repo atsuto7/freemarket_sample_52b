fa0252cd5c08cbb4a4d9c1db5895698ca8fc51ab
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-15 18:58:59 +0900

    remove unnecessary lines

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {
-    confirmations: 'users/confirmations'
-    omniauth_callbacks: 'users/omniauth_callbacks',
-    passwords: 'users/passwords'
     registrations: 'users/registrations'
-    sessions: 'users/sessions'
-    unlocks: 'users/unlocks'
   }
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'templates#index'
