69723ad768df667dce50de5e1decf984c6c898c1
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-16 20:48:05 +0900

    create Users table

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'templates#index'
   get '/templates/show' => 'templates#show'
