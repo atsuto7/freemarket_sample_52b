9f2fbb94dc0658849a1aa2dc298d7c3674cf3575
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-16 21:13:18 +0900

    create routes for registration

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  devise_for :users, controllers: {
-    omniauth_callbacks: 'users/omniauth_callbacks',
+  devise_for :users, skip: :all, controllers: {
     registrations: 'users/registrations'
-    sessions: 'users/sessions'
   }
+
+  devise_for :user do
+    # とりあえずregistrationのみ
+    get    'signup/registration', to: 'devise/registrations#new'
+    post   'signup/registration', to: 'devise/registrations#create'
+    put    'users',               to: 'devise/registrations#update'
+    patch  'users',               to: 'devise/registrations#update'
+    delete 'users',               to: 'devise/registrations#destroy'
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'templates#index'
   get '/templates/show' => 'templates#show'
@@ -12,12 +20,6 @@ Rails.application.routes.draw do
   get '/templates/login' => 'templates#login'
   get '/templates/mypage' => 'templates#mypage'
   get '/templates/productsinformation' => 'templates#productsinformation'
-  get '/templates/topuserregistar' => 'templates#topuserregistar'
-  get '/templates/userregistar1' => 'templates#userregistar1'
-  get '/templates/userregistar2' => 'templates#userregistar2'
-  get '/templates/userregistar3' => 'templates#userregistar3'
-  get '/templates/userregistar4' => 'templates#userregistar4'
-  get '/templates/userregistar5' => 'templates#userregistar5'
   get '/templates/view3mypagehome' => 'templates#view3mypagehome'
   get '/templates/view3profile' => 'templates#view3profile'
   get '/templates/views3identification' => 'templates#views3identification'
