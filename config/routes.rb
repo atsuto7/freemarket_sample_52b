97bb70d7eca57ada732fea4700299f68861c28c1
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-16 16:32:02 +0900

    uninstall devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,21 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users, skip: :all, controllers: {
-    registrations: 'users/registrations'
-  }
- 
-  devise_scope :user do
-    # とりあえずregistrationのみ
-    get    'signup',                 to: 'users/registrations#index'
-    get    'signup/registration',    to: 'users/registrations#new',       as: :new_user_registration
-    post   'signup/registration',    to: 'users/registrations#create',    as: :user_registration
-    get    'signup/smsconfirmation', to: 'users/registrations#sms',       as: :new_user_registration_sms
-    get    'signup/adress',          to: 'users/registrations#adress',    as: :new_user_registration_adress
-    get    'signup/payment',         to: 'users/registrations#payment',   as: :new_user_registration_payment
-    get    'signup/confirmed',       to: 'users/registrations#confirmed', as: :new_user_registration_confirmed
-    put    'users',                  to: 'users/registrations#update'
-    patch  'users',                  to: 'users/registrations#update'
-    delete 'users',                  to: 'users/registrations#destroy'
-  end
 
   root 'templates#index'
   get '/templates/show' => 'templates#show'
