da63ddc135d816e7e2641bd3f0e1b2c50cb3c9d2
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-17 19:23:33 +0900

    adjust view files for registration and sign in

--- config/routes.rb
+++ config/routes.rb
@@ -3,25 +3,26 @@ Rails.application.routes.draw do
     controllers: { registrations: 'users/registrations' }
 
   devise_scope :user do
-    # とりあえずregistrationのみ
+    # registration用
     get    'signup',              to: 'devise/registrations#index'
     get    'signup/registration', to: 'devise/registrations#new', as: :new_user_registration
     post   'signup/registration', to: 'devise/registrations#create', as: :user_registration
     put    'users',               to: 'devise/registrations#update'
     patch  'users',               to: 'devise/registrations#update'
-    delete 'users',               to: 'devise/registrations#destroy'
+    # session用
+    get 'login' => 'devise/sessions#new', as: :new_user_session
+    post 'login' => 'devise/sessions#create', as: :user_session
+    get 'signout',         to: 'devise/sessions#destroy', as: :destroy_user_registration
   end
 
   root 'templates#index'
   get '/templates/show' => 'templates#show'
   get '/templates/purchaseconfirmation' => 'templates#purchaseconfirmation'
-  get '/templates/login' => 'templates#login'
   get '/templates/mypage' => 'templates#mypage'
   get '/templates/productsinformation' => 'templates#productsinformation'
   get '/templates/view3mypagehome' => 'templates#view3mypagehome'
   get '/templates/view3profile' => 'templates#view3profile'
   get '/templates/views3identification' => 'templates#views3identification'
-  get '/templates/views3logout' => 'templates#views3logout'
   get '/templates/views3payment' => 'templates#views3payment'
   get '/templates/sellpage' => 'templates#sellpage'
   resources :products do
