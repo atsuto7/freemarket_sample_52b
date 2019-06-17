ce9f4ce5f85f010cfc3873c7ad03a16352294438
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-17 19:31:40 +0900

    fix routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,14 +5,14 @@ Rails.application.routes.draw do
   devise_scope :user do
     # registration用
     get    'signup',              to: 'devise/registrations#index'
-    get    'signup/registration', to: 'devise/registrations#new', as: :new_user_registration
+    get    'signup/registration', to: 'devise/registrations#new',    as: :new_user_registration
     post   'signup/registration', to: 'devise/registrations#create', as: :user_registration
     put    'users',               to: 'devise/registrations#update'
     patch  'users',               to: 'devise/registrations#update'
     # session用
-    get 'login' => 'devise/sessions#new', as: :new_user_session
-    post 'login' => 'devise/sessions#create', as: :user_session
-    get 'signout',         to: 'devise/sessions#destroy', as: :destroy_user_registration
+    get    'login',               to: 'devise/sessions#new',         as: :new_user_session
+    post   'login',               to: 'devise/sessions#create',      as: :user_session
+    get    'logout',              to: 'devise/sessions#destroy',     as: :destroy_user_registration
   end
 
   root 'templates#index'
