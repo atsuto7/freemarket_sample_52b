1a412769628eaeb961490022ca35d9447fc8e5b4
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-19 19:53:42 +0900

    add accosiations between user and address

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,5 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  has_one :address
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
