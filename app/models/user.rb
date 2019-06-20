d7ed67ef31134998aa5b6df3ee415bbe23df04b8
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-20 21:07:21 +0900

    add validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,8 +7,15 @@ class User < ApplicationRecord
 
   accepts_nested_attributes_for :address
 
-  validates :nickname, presence: true, length: { maximum: 20 }
+  reg_kana = /\A[ァ-ヴ]+\z/
+
+  validates :nickname, presence: true, uniqueness: true, length: { maximum: 20 }
   validates :password, length: { in: 6..128 }, allow_blank: true
+  validates :phone_number, presence: true
+  validates :kanji_surname, presence: true
+  validates :kanji_name, presence: true
+  validates :kana_surname, format: { with: reg_kana }, presence: true
+  validates :kana_name, format: { with: reg_kana }, presence: true
 
   def with_address
     build_address if address.nil?
