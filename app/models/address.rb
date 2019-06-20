a19482ca6aab9fe4dfa2ec90c6ca872ef0c9f0ae
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-20 21:13:48 +0900

    add validations to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,13 @@
 class Address < ApplicationRecord
   belongs_to :user
+
+  reg_postal_code = /\A[0-9]{3}\-[0-9]{4}+\z/
+
+  validates :postal_code, presence: true, format: { with: reg_postal_code }
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :street_number, presence: true
+  validates :building, allow_blank: true
+  validates :home_number, allow_blank: true
+
 end
