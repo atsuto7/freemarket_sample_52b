9ee322af46c6af7862cda338286d2764e2adecfc
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-24 15:55:35 +0900

    add omniauth_callbacks to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, skip: :all, 
-    controllers: { registrations: 'users/registrations' }
+    controllers: { 
+    registrations: 'users/registrations',
+    omniauth_callbacks: 'users/omniauth_callbacks' 
+    }
 
   devise_scope :user do
     # registration用
