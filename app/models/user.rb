f416ce08c3cbbbeab0eb410ae092f5e82a092568
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-24 16:02:46 +0900

    define self.find_for_oauth

--- app/models/user.rb
+++ app/models/user.rb
@@ -24,4 +24,19 @@ class User < ApplicationRecord
     self
   end
 
+  def self.find_for_oauth(auth)
+    user = User.where(uid: auth.uid, provider: auth.provider).first
+
+    unless user
+      user = User.create(
+        uid:      auth.uid,
+        provider: auth.provider,
+        nickname:     auth.info.name,
+        email:    auth.email,
+        password: Devise.friendly_token[0, 20]
+      )
+    end
+
+  user
+
 end
