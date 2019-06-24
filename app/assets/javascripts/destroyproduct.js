c2202c7b1b6912c659959b564064c51f17b39d32
atsuto <g.atsuto@icloud.com>
2019-06-24 16:06:48 +0900

    削除機能jsアラート

---
+++ app/assets/javascripts/destroyproduct.js
@@ -0,0 +1,13 @@
+$(function() {
+  $(document).on('turbolinks:load', function(){
+$('.products-information__checkout-btn__delete').click(function(){
+  if(!confirm('本当に削除しますか？')){
+      /* キャンセルの時の処理 */
+      return false;
+  }else{
+      /*　OKの時の処理 */
+      location.href = '/';
+  }
+});
+  })
+})
