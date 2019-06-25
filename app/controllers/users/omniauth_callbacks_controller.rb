c9191cb3e217cecd5641e5f5989bd194b84f69d8
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-25 14:45:06 +0900

    Update OmniauthCallbacksController

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,23 +1,18 @@
-# frozen_string_literal: true
-
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
 
-  def facebook
-    callback_from :facebook
+  def facebook_callback
+    callback_for(:facebook)
   end
 
-  def google_oauth2
-    callback_from :google
+  def google_oauth2_callback
+    callback_for(:google)
   end
 
-  private
-
-  def callback_from(provider)
+  def callback_for(provider)
     provider = provider.to_s
-    @user = User.find_for_oauth(request.env['omniauth.auth'])
+    @user = User.from_omniauth(request.env['omniauth.auth'])
 
     if @user.persisted?
-      flash[:notice] = I18n.t('devise.omniauth_callbacks.success', kind: provider.capitalize)
       sign_in_and_redirect @user, event: :authentication
     else
       session[:nickname] = @user.nickname
@@ -25,8 +20,12 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
       session[:password] = @user.password
       session[:provider] = @user.provider
       session[:uid] = @user.uid
-      redirect_to new_user_registration_sns_path
+      redirect_to new_user_registration_url
     end
   end
-   
+
+  def failure
+    redirect_to root_path
+  end
+
 end
