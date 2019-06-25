be657e2231674de98d2b4b95747da79557de2453
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-25 15:37:28 +0900

    add validations to prefecture_id

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,8 +1,10 @@
 class Address < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :user
+  belongs_to_active_hash :prefecture
 
   validates :postal_code, presence: true
-  validates :prefecture, presence: true
+  validates :prefecture_id, presence: true, numericality: { greater_than: 0 }
   validates :city, presence: true
   validates :street_number, presence: true
 
