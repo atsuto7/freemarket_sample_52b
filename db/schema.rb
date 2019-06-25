a841b636b5be0298945d202fa3c7a0979417f298
Kiyoka Ishikawa <kiyonfu@gmail.com>
2019-06-25 15:36:16 +0900

    rename column to product_id

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_24_061430) do
+ActiveRecord::Schema.define(version: 2019_06_25_061403) do
 
   create_table "addresses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.integer "postal_code", null: false
-    t.integer "prefecture", null: false
+    t.integer "prefecture_id", null: false
     t.string "city", null: false
     t.string "street_number", null: false
     t.string "building"
