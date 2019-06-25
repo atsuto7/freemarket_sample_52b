b67b59ba3d1478ec613b0d75f771fb690c6461a2
atsuto <g.atsuto@icloud.com>
2019-06-25 12:07:32 +0900

    商品詳細画面複数画像表示

---
+++ app/assets/javascripts/productshow.js
@@ -0,0 +1,21 @@
+$(function() {
+  $(document).on('turbolinks:load', function(){
+    var slideWidth = $('.products-information__item__image__slideset__slide').outerWidth();
+    var slideNum = $('.products-information__item__image__slideset__slide').length;  
+    var slideSetWidth = slideWidth * slideNum; 
+    $('.products-information__item__image__slideset').css('width', slideSetWidth); 
+    $('.products-information__item__image__selector__photo').on('mouseover', function(){
+      var index = $('.products-information__item__image__selector__photo').index(this);
+      timeout = setTimeout($.proxy(function(){
+      $('.products-information__item__image__selector__photo').css('opacity', 0.4); 
+      $(this).css('opacity', 1); 
+      $('.products-information__item__image__slideset').animate({
+        left: -slideWidth * index
+      });
+    }, this),500);
+    $('.products-information__item__image__selector__photo').on('mouseout', function(setTimeout){
+      clearTimeout(timeout);
+    })
+    });
+  })
+})
